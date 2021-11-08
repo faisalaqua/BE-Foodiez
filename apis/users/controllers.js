f69fc7492e1b9f528693bac528f19f876d26e5b1
Your Name <you@example.com>
2021-11-08 16:30:25 +0300

    testing branch

---
+++ apis/users/controllers.js
@@ -0,0 +1,23 @@
+const User = require("../../db/models/User");
+const bcrypt = require("bcrypt");
+const { JWT_SECRET, JWT_EXPIRATION_MS } = require("../../config/keys");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+    exp: Date.now() + JWT_EXPIRATION_MS,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET);
+  return token;
+};
+
+exports.signup = async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const user = await User.create;
+  } catch (error) {}
+};
